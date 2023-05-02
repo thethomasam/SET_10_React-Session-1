57e8c678253d3e3cfb4a7a7afa79463c3fc91d40
thethomasam <samkoshythomas007@gmail.com>
2023-05-02 17:40:42 +0930

    session-1-code

---
+++ src/counter.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+
+function Counter() {
+    const [count, setCount] = useState(0);
+
+    const handleIncrement = () => {
+        setCount(count + 1);
+    };
+
+    const handleDecrement = () => {
+        setCount(count - 1);
+    };
+
+    return (
+        <div>
+            <h1>Counter</h1>
+            <p>Count: {count}</p>
+            <button onClick={handleIncrement}>Increment</button>
+            <button onClick={handleDecrement}>Decrement</button>
+        </div>
+    );
+}
+
+export default Counter;
